28c512629311c3e734de205d8d9f82d9d970349a
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-11-05 14:53:09 -0500

    finished

--- lab3/wia1927_LAB3.py
+++ lab3/wia1927_LAB3.py
@@ -5,6 +5,10 @@
 # Python Ver   : Python 3.8.10
 
 # Define basic operations
+from fileinput import filename
+from unittest import result
+
+
 def plus(a, b):
     return a + b
 
@@ -31,21 +35,43 @@ def scan_input(char_stream):
 
     for token in split_stream:
         if (token.isdigit() == False or len(token) != 1) and (token not in op_dict):
-            print(token + " is not a valid token")
+            split_stream = "[ERROR] '" + token + "' is not a valid token"
         
     return split_stream
 
-# TODO: make sure to check if its a valid sequence of tokens
-# iterates through the token stream and apply operations
 def parse_input(token_stream):
+    # Test 1: Not enough tokens for a single expression
+    if len(token_stream) < 3:
+        return "[ERROR] Not enough tokens (at least 3)"
+
+    # Test 2: Operator before 2 operands
+    if not token_stream[0].isdigit() or not token_stream[1].isdigit():
+        return "[ERROR] Operators must have 2 operands"
+
+    # Test 3: Incorrect number of operators
+    operands = list(filter(lambda token: token.isdigit(), token_stream))
+
+    if (len(token_stream) -  len(operands)) > (len(operands) - 1):
+        result = "[ERROR] Too many operators"
+    elif (len(token_stream) - len(operands)) < (len(operands) - 1):
+        result = "[ERROR] Not enough operators"
+    else:
+        result = "all good"
+
+    return result
+            
+
+# iterates through the token stream and apply operations
+def evaluate(token_stream, verbose=False):
     operands = []
 
     for token in token_stream:
         # if token is an operand, push to stack
-        if token.isdigit() == True:
+        if token.isdigit():
             operands.append(token)
 
-            print(operands, " <- ", token)
+            if verbose:
+                print(operands, " <- ", token)
         # if token is operation, apply operation to the top 2 operands in the stack and push result on that same stack
         else:
             # obtain operation function from dictionary
@@ -60,22 +86,31 @@ def parse_input(token_stream):
             # push result of operation to operands stack
             operands.append(result)
 
-            print(operands, " <- ", a, token, b)
+            if verbose:
+                print(operands, " <- ", a, token, b)
     
     return operands
 
-# Example inputs
-scanned_line = "6 5 + 4 * 3 2 + 1 * /"
-canvas_example = "4 2 5 * + 1 3 2 * + /"
+def solve_RPN(char_stream):
+    # make sure all tokens are valid tokens
+    token_stream = scan_input(char_stream)
 
-# make sure all tokens are valid tokens
-token_stream = scan_input(scanned_line)
+    if "ERROR" in token_stream:
+        result = [token_stream]
+    else:
+        parse_result = parse_input(token_stream)
+        
+        if "ERROR" in parse_result:
+            result = [parse_result]
+        else:
+            # make sure all token sequences are valid, and apply operations in input
+            result = evaluate(token_stream)
 
-# make sure all token sequences are valid, and apply operations in input
-result = parse_input(token_stream)
+    return result[0]
 
-print(result)
 
-# read line
-# get result from line
-# write line
+input= open("input_RPN.txt", "r")
+
+for expression in input:
+    result = str(solve_RPN(expression.strip('\n')))
+    print(result)
